a2ad81fb152bbfced0e902f19782ff55dbba828f
Bornemissz Zsigmond <bornemissza.zsigmond@ganziskola.hu>
2020-10-31 15:03:42 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test_2019VisualStudio_CSharpGitDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+            Console.ReadKey();
+        }
+    }
+}
